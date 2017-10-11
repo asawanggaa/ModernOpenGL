98ed95ac1a80f311a3d210c315c06206175c2a04
asawanggaa <asawanggaa@gmail.com>
2017-10-11 21:21:43 +0800

    seting enviroment

---
+++ ModernOpenGL/ModernOpenGL.cpp
@@ -0,0 +1,71 @@
+#include "stdafx.h"
+#include <iostream>
+
+#include <GL\glew.h>
+#include <GL\glut.h>
+#include <GLFW\glfw3.h>
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		system("pause");
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height) 
+	{
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glViewport(0, 0, width, height); 
+	});
+	glfwSetKeyCallback(window, [](GLFWwindow* window,int key,int scancode,int action,int mods) 
+	{
+		if (action != GLFW_PRESS)
+			return;
+		switch (key)
+		{
+		case GLFW_KEY_ESCAPE:
+			glfwSetWindowShouldClose(window, GL_TRUE);
+			break;
+		default:
+			break;
+		}
+	});
+
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (err != GLEW_OK) {
+		std::cerr << "Failed to Init GLEW" << std::endl;
+		system("pause");
+		return -1;
+	}
+
+	
+	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+	
+	while (!glfwWindowShouldClose(window)) {
+
+
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+    return 0;
+}
