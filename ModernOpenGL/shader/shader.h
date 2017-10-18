8bdb27af42926f43171f8249681b5b1e86cc6812
asawanggaa <asawanggaa@gmail.com>
2017-10-19 00:54:22 +0800

    Texture Boxes

--- ModernOpenGL/shader/shader.h
+++ ModernOpenGL/shader/shader.h
@@ -4,19 +4,24 @@ const char *vertexShaderSource =
 "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "layout (location = 1) in vec3 aColor;\n"
+"layout (location = 2) in vec2 aTexCoord;\n"
 "out vec3 vertexColor;\n"
+"out vec2 TexCoord;\n"
 "uniform vec2 movement;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x+movement.x, aPos.y+movement.y, aPos.z, 1.0f);\n"
+"   gl_Position = vec4(aPos, 1.0f);\n"
 "	vertexColor = aColor;\n"
+"	TexCoord = aTexCoord;\n"
 "}\0";
 
 const char *fragmentShaderSource =
 "#version 330 core\n"
 "out vec4 FragColor;\n"
 "in vec3 vertexColor;\n"
+"in vec2 TexCoord;\n"
+"uniform sampler2D ourTexture;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(vertexColor, 1.0f);\n"
+"   FragColor = texture(ourTexture, TexCoord);\n"
 "}\n\0";
